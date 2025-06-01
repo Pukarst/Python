8dce23e9a3a042e70955a09b4a608dbfe8075f4f
pukar_bade <bade5pukar@gmail.com>
2025-06-01 23:05:50 +0545

    python intro

---
+++ day_1.py
@@ -0,0 +1,26 @@
+'''
+--> Some of the basic information of Python<--
+
+--Python is the popular programming language
+created by Guido Van Rossum in 1991.
+--Python is used in the different sector 
+which are mentioned below:
+    >Web Development
+    >Data Science & Analytics
+    >Machine Learning & Artificial Intelligence
+    >Automation & Scripting
+    >Game Development
+    >Internet Of Things,etc
+--Why Python???
+    >Easy to learn and read
+    >Versatile and Powerful
+    >Rich Ecosystem of Libraries and Framework
+    >Cross-Platform Compatibility
+    >Great for Rapid Development,etc
+'''
+
+print('''
+        Today, I’ve started  60-day learning 
+        challenge, starting my journey with Python. 
+        Excited to see where this path takes me!
+      ''')
