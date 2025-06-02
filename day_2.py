9c8dced8feab5c0a0100de8e5326aefc9fa6893f
pukar_bade <bade5pukar@gmail.com>
2025-06-02 22:29:42 +0545

    variables

---
+++ day_2.py
@@ -0,0 +1,19 @@
+# Today I am learning about the variables in Python.
+
+a=2     # this is integer 
+b='day' # this is string
+print(type(b))  # this is used to know the variable type
+print(b,a)
+print(f"{b} {a} of learning python")
+
+c,d,e='learning','the','Python'
+print(c+d+e)
+
+language=['C','C++','Python']
+f,g,h=language
+print(f'The programming languages are: {f},{g},{h}.')
+
+i=15
+j=10
+sum=(i+j)
+print(f'The addition of the two number are:{sum}')
